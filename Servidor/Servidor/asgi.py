5024408350d901471db62714fdcc59742e01abe1
Nico <ni.navarroa@duocuc.cl>
2023-10-10 18:58:19 -0300

    ProyectoDjango

--- Servidor/Servidor/asgi.py
+++ Servidor/Servidor/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Clinica project.
+ASGI config for Servidor project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Clinica.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Servidor.settings')
 
 application = get_asgi_application()
--- Servidor/Servidor/wsgi.py
+++ Servidor/Servidor/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Clinica project.
+WSGI config for Servidor project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Clinica.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Servidor.settings')
 
 application = get_wsgi_application()
