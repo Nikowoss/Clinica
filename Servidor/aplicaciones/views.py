5e89a48d5a357130a2daea6c1bfcd7557ef123de
Nico <ni.navarroa@duocuc.cl>
2023-10-13 16:16:18 -0300

    sumandoPaginas

--- Servidor/aplicaciones/views.py
+++ Servidor/aplicaciones/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def Inicio(request):
-    return render(request,'aplicaciones/Inicio.html')
+def InicioSesion(request):
+    return render(request,'aplicaciones/InicioSesion.html')
+
+def CrearCuenta(request):
+    return render(request,'aplicaciones/CrearCuenta.html')
+    
+def InicioPaciente(request):
+    return render(request,'aplicaciones/InicioWebPaciente.html')
+
+def HoraMedica(request):
+    return render(request,'aplicaciones/HoraMedica.html')
+
