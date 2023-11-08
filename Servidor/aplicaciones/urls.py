b868871d526d0705b50d505130aa9ebbd3e0b66b
Nico <ni.navarroa@duocuc.cl>
2023-11-08 12:41:20 -0300

    arreglos

--- Servidor/aplicaciones/urls.py
+++ Servidor/aplicaciones/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-<<<<<<< HEAD
 from .views import login,InicioPaciente,enviar_cliente_a_api,disponibilidad,loginMedico,CDPrueba,HoraMedica,HoraDisponible,verPacientes,vistaMedico,agenda,agregaragenda
-=======
-from .views import enviar_correo, login,InicioPaciente,enviar_cliente_a_api,HoraMedica,HoraDisponible,verPacientes,vistaMedico,disponibilidad
->>>>>>> seeeb
 
 urlpatterns = [
     path('InicioSesion', login,name="InicioSesion"),
@@ -14,17 +10,10 @@ urlpatterns = [
     #path('probandoapirest',probandoapirest,name="probandoapirest"),
     path('verPacientes',verPacientes, name='VerPacientes'),
     path('VistaMedico', vistaMedico, name='VistaMedico'),
-<<<<<<< HEAD
     path('agenda', agenda, name='agenda'),
     path('agregaragenda', agregaragenda, name='agregaragenda'),
     path('CDPrueba', CDPrueba, name='CDPrueba'),
     path('InicioMedico', loginMedico, name='InicioMedico'),
     path('disponibilidad',disponibilidad,name='disponibilidad'),
     path('cargar-excel/', disponibilidad, name='cargar_excel'),
-=======
-    path('disponibilidad',disponibilidad,name='disponibilidad'),
-    path('cargar-excel/', disponibilidad, name='cargar_excel'),
-    path('EnviarCorreo',enviar_correo, name='enviar_correo'),
-
->>>>>>> seeeb
 ]
