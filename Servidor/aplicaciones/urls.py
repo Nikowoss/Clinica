1d1a16dedc89e7f169dc07d58d8e72aa5ad8e3b8
Gabylina <gab.sandoval@duocuc.cl>
2023-11-08 10:54:19 -0300

    Inicio sesión Medico

--- Servidor/aplicaciones/urls.py
+++ Servidor/aplicaciones/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import login,InicioPaciente,enviar_cliente_a_api,HoraMedica,HoraDisponible,verPacientes,vistaMedico
+from .views import login, InicioPaciente, enviar_cliente_a_api, HoraMedica, HoraDisponible, verPacientes, loginMedico, InicioMedico, VistaMedico
 
 urlpatterns = [
-    path('InicioSesion', login,name="InicioSesion"),
-    path('', InicioPaciente,name="InicioPaciente"),
-    path('CrearCuenta',enviar_cliente_a_api,name="CrearCuenta"),
-    path('HoraMedica',HoraMedica,name="HoraMedica"),
-    path('HoraDisponible',HoraDisponible,name="HoraDisponible"),
+    path('InicioSesion', login, name="InicioSesion"),
+    path('', InicioPaciente, name="InicioPaciente"),
+    path('CrearCuenta', enviar_cliente_a_api, name="CrearCuenta"),
+    path('VistaMedico', VistaMedico, name='VistaMedico'),
+    path('HoraMedica', HoraMedica, name="HoraMedica"),
+    path('HoraDisponible', HoraDisponible, name="HoraDisponible"),
+    path('verPacientes', verPacientes, name='VerPacientes'),
+    path('InicioMedico', loginMedico, name='InicioMedico'),
     #path('probandoapirest',probandoapirest,name="probandoapirest"),
-    path('verPacientes',verPacientes, name='VerPacientes'),
-    path('VistaMedico', vistaMedico, name='VistaMedico'),
+    # path('obtener_medicos_y_centros/<int:especialidad_id>/', views.obtener_medicos_y_centros, name='obtener_medicos_y_centros'),
 ]
--- Servidor/aplicaciones/views.py
+++ Servidor/aplicaciones/views.py
@@ -7,41 +7,11 @@ from django.contrib import messages
 # Create your views here.
 
 
-def InicioSesion(request):
-    """  if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            data = {
-                'rutPaciente': rutPaciente,
-                'contraPaciente': contraPaciente
-            }
-            url_api_replit = 'https://api-tareas.nicon607.repl.co/api/Paciente/'
-
-            response = requests.post(url_api_replit, json=data)
-
-            # Obtén los datos del formulario
-            rutPaciente = form.cleaned_data['rutPaciente']
-            contraPaciente = form.cleaned_data['contraPaciente']
-
-            # Realiza una solicitud a tu API para verificar el inicio de sesión
-            
-            response = requests.post(url_api_replit, json=data)
-
-            if response.status_code == 200:
-                # El inicio de sesión fue exitoso
-                # Puedes personalizar la lógica para manejar el inicio de sesión
-                return render(request,'InicioWebPaciente.html')  # Redirige a la página de inicio
-            else:
-                # El inicio de sesión falló
-                # Puedes mostrar un mensaje de error en el formulario o redirigir a una página de error
-                return render(request, 'InicioSesion.html')
-    else:
-        form = LoginForm()
-"""
-    return render(request, 'aplicaciones/InicioSesion.html')
-
 def CrearCuenta(request):
     return render(request,'aplicaciones/CrearCuenta.html')
+    
+def InicioMedico(request):
+    return render(request,'aplicaciones/InicioMedico.html')    
     
 def InicioPaciente(request):
     return render(request,'aplicaciones/InicioWebPaciente.html')
@@ -54,8 +24,11 @@ def HoraDisponible(request):
 
 def CrearCuenta(request):
     return render(request,'aplicaciones/CrearCuenta.html')
-def vistaMedico(request):
-    return render(request, 'aplicaciones/vistaMedico.html')
+
+def VistaMedico(request):
+    correo = request.session.get('correo')
+    return render(request, 'aplicaciones/VistaMedico.html', {'correo': correo})
+
 
 def verPacientes(request):
     url_api_replit = 'https://api-tareas.nicon607.repl.co/api/Paciente/'  # Reemplaza con la URL real
@@ -146,3 +119,130 @@ def login(request):
         except Exception as ex:
             print("Error en :", ex)
     return render(request,'aplicaciones/InicioSesion.html')
+
+
+def getEspecialidades(request):
+    api_url = 'https://api-tareas.nicon607.repl.co/api/Especialidad'
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        especialidades = [{'id': especialidad['idespecialidad'], 'nombre': especialidad['nomespecialidad']} for especialidad in data]
+        print("Especialidades:", especialidades)
+        return especialidades
+    else:
+        return []
+
+    
+
+def getMedicos(request):
+    api_url = 'https://api-tareas.nicon607.repl.co/api/Medico'
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        medicos = [{'nombre': medico['nommedico'], 'apellido': medico['apemedico']} for medico in data]
+        print("Nombres de Medicos:", medicos)
+        return medicos
+    else:
+        return []
+
+
+def getCentros(request):
+    api_url = 'https://api-tareas.nicon607.repl.co/api/Centro'
+    response = requests.get(api_url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        nombres_centros = [( centro['nombrecentro']) for centro in data]
+        print("Nombres de centros:", nombres_centros)
+        return nombres_centros
+    else:
+        return []
+
+
+def HoraMedica(request):
+    especialidades = getEspecialidades(request)
+    medicos = getMedicos(request)
+    centros = getCentros(request)
+    return render(request, 'aplicaciones/HoraMedica.html', {'especialidades': especialidades, 'medicos': medicos, 'centros': centros})
+
+
+
+# # Vistas en Django
+# from django.http import JsonResponse
+
+# def obtener_medicos_y_centros(request, idespecialidad):
+#     medicos = obtener_medicos_por_especialidad(idespecialidad)
+#     centros = obtener_centros_por_especialidad(idespecialidad)
+
+#     data = {
+#         'medicos': medicos,
+#         'centros': centros,
+#     }
+
+#     return JsonResponse(data)
+
+# def obtener_medicos_por_especialidad(idespecialidad):
+#     api_url = f'https://api-tareas.nicon607.repl.co/api/Medico?especialidad_idespecialidad={idespecialidad}'  # Asegúrate de usar el nombre correcto del campo
+#     response = requests.get(api_url)
+
+#     if response.status_code == 200:
+#         data = response.json()
+#         medicos = [{'rutmedico': medico['rutmedico'], 'nombre': medico['nommedico']} for medico in data]
+#         return medicos
+#     else:
+#         return []
+
+# def obtener_centros_por_especialidad(especialidad_id):
+#     api_url = f'https://api-tareas.nicon607.repl.co/api/Centro?especialidad_idcentro={especialidad_id}'  # Asegúrate de usar el nombre correcto del campo
+#     response = requests.get(api_url)
+    
+#     if response.status_code == 200:
+#         data = response.json()
+#         centros = [{'idcentro': centro['idcentro'], 'nombrecentro': centro['nombrecentro']} for centro in data]
+#         return centros
+#     else:
+#         return []
+
+
+def loginMedico(request):
+    print("a")
+    api_url = 'https://api-tareas.nicon607.repl.co/api/Medico/login'
+
+    print("Vista de inicio de sesión está siendo ejecutada")
+
+    if request.method == 'POST':
+        print("metodo post")
+        usuario_data = {
+            "correoMedico" : str(request.POST.get('correoMedico')),
+            "contramedico" : str(request.POST.get('contramedico')),
+
+    }
+        print("Datos del usuario:", usuario_data)
+
+        data_json = json.dumps(usuario_data)
+
+        headers = {'Content-Type' : 'application/json'}
+
+        try:
+            response = requests.post(api_url, data=data_json, headers=headers)
+            print(response)
+            print("Estado de la respuesta:", response.status_code)
+            if response.status_code == 200:
+                respuesta = response.json()
+
+                if respuesta.get("message") :
+                    messages.warning(request, "Error al iniciar sesión")
+                else:
+                    correo = respuesta.get("correoMedico")
+                    request.session['correo'] = correo
+                    print("Inicio de sesión correcto")
+                    print(correo)
+                    print("Respuesta de la API:", respuesta)
+                    return redirect('VistaMedico')
+            else:
+                print("Credenciales inválidas")
+        except Exception as ex:
+            print("Error en :", ex)
+    return render(request,'aplicaciones/InicioMedico.html')
