b7c0b9c9fc81641c7d887330920f56a14d77f40b
Seba <se.bernal@duocuc.cl>
2023-12-02 12:33:05 -0300

    incremento id disponibilidad

--- Servidor/aplicaciones/views.py
+++ Servidor/aplicaciones/views.py
@@ -67,7 +67,8 @@ def disponibilidad(request):
                 if ids_v2:
                     print("error aca antes")
                     y = max(ids_v2)
-                    print("error aca")
+                    y = y + 1
+                    print(y)
                 else:
                     y = 1
                     print(y)
@@ -85,69 +86,70 @@ def disponibilidad(request):
                         rut_med_C = r
                     else:
                         rut_med_C = 0
-            if rut_med_C == rut :
-                #recorrer la matriz y mandar datos al replit
-                for lista in filas_lista:
-                    x = x + 1
-                    lista_def = []
-                    #print(lista)
-                    #print(x)
-                    lista_def.append(x)
-                    lista_def.append(str(lista[2]))
-                    lista_def.append(str(lista[3]))
-                    
-                    #print(lista_def)
-                    #tabla modulo
-                    modulo_data = {
-                        "idmodulo" : x,
-                        "horaInicio" : str(lista[2]),
-                        "horaFinal" : str(lista[3])
+            """ if rut_med_C == rut : """
+            #recorrer la matriz y mandar datos al replit
+            for lista in filas_lista:
+                x = x + 1
+                print(x , y )
+                lista_def = []
+                #print(lista)
+                #print(x)
+                lista_def.append(x)
+                lista_def.append(str(lista[2]))
+                lista_def.append(str(lista[3]))
+                
+                #print(lista_def)
+                #tabla modulo
+                modulo_data = {
+                    "idmodulo" : x,
+                    "horaInicio" : str(lista[2]),
+                    "horaFinal" : str(lista[3])
+                }
+                #tabla disponibilidad
+                dispo_data = {
+                    "id_disponibilidad" : y,
+                    "modulo_idmodulo" : x,
+                    "fechaDispon" : str(lista[1]),
+                    "disponible" : str(lista[0]),
+                    "rut_medico" : rut
                     }
-                    #tabla disponibilidad
-                    dispo_data = {
-                        "id_disponibilidad" : y,
-                        "modulo_idmodulo" : x,
-                        "fechaDispon" : str(lista[1]),
-                        "disponible" : str(lista[0]),
-                        "rut_medico" : rut
-                        }
+ 
+                # Convertir la lista de diccionarios a JSON
+                lista_def_json = json.dumps(modulo_data)
+                dispo_json = json.dumps(dispo_data)
 
-                    # Convertir la lista de diccionarios a JSON
-                    lista_def_json = json.dumps(modulo_data)
-                    dispo_json = json.dumps(dispo_data)
+                # Enviar lista_def_json a la API
+                api_response = requests.post(url_api_replit, data=lista_def_json, headers={'Content-Type': 'application/json'})
+                # enviar dispo_json a la API
+                api_res = requests.post(url_api, data=dispo_json, headers={'Content-Type': 'application/json'})
+                #desde que se agrego el rut_medico se marca error con la API, puede ser que no lo recibe bien la api
+                # por tipo de dato, o que no se envia bien a la BD (sirve borrar la columna y volverla a crear) 
+                
+                print("Respuesta API Modulo:", api_response.status_code)
+                print("Respuesta API Disponibilidad:", api_res.status_code)
 
-                    # Enviar lista_def_json a la API
-                    api_response = requests.post(url_api_replit, data=lista_def_json, headers={'Content-Type': 'application/json'})
-                    # enviar dispo_json a la API
-                    api_res = requests.post(url_api, data=dispo_json, headers={'Content-Type': 'application/json'})
-                    #desde que se agrego el rut_medico se marca error con la API, puede ser que no lo recibe bien la api
-                    # por tipo de dato, o que no se envia bien a la BD (sirve borrar la columna y volverla a crear) 
-                    
-                    print("Respuesta API Modulo:", api_response.status_code)
-                    print("Respuesta API Disponibilidad:", api_res.status_code)
-
-                    if api_res.status_code == 200:
-                        print(api_res.status_code)
-                    else:
-                        print(api_res.status_code)
-                        print("respuesta de disponibilidad mala")
-                    if api_response.status_code == 200:
-                        print(api_response.status_code)
-                    else:
-                        print(api_response.status_code)
-                        print("respuesta de modulo mala")
-                    
-                # Convierte el DataFrame en una tabla HTML
-                if api_response.status_code == 200:
-                    return render(request, 'aplicaciones/resultado.html', {'data': df_mostrar.to_html(classes='table table-bordered', escape=False, index=False)})
+                if api_res.status_code == 200:
+                    print(api_res.status_code)
                 else:
-                    messages.warning(request, f"Error al enviar datos a la API. Código de estado: {api_response.status_code}")
-                tabla_html = df_mostrar.to_html(classes='table table-bordered', escape=False, index=False)
-
-                return render(request, 'aplicaciones/resultado.html', {'data': tabla_html, 'filas_lista': filas_lista})
+                    print(api_res.status_code)
+                    print("respuesta de disponibilidad mala")
+                if api_response.status_code == 200:
+                    print(api_response.status_code)
+                else:
+                    print(api_response.status_code)
+                    print("respuesta de modulo mala")
+                
+            # Convierte el DataFrame en una tabla HTML
+            if api_response.status_code == 200:
+                return render(request, 'aplicaciones/resultado.html', {'data': df_mostrar.to_html(classes='table table-bordered', escape=False, index=False)})
             else:
+                messages.warning(request, f"Error al enviar datos a la API. Código de estado: {api_response.status_code}")
+            tabla_html = df_mostrar.to_html(classes='table table-bordered', escape=False, index=False)
+
+            return render(request, 'aplicaciones/resultado.html', {'data': tabla_html, 'filas_lista': filas_lista})
+            """ else:
                 print("rut no valido")
-                return render(request, 'aplicaciones/error.html', {'error_message': 'El rut ingresado en el excel no esta registrado .'})
+                return render(request, 'aplicaciones/error.html', {'error_message': 'El rut ingresado en el excel no esta registrado .'}) """
         else:
             return render(request, 'aplicaciones/error.html', {'error_message': 'El archivo no es un archivo Excel válido.'})
     return render(request, 'aplicaciones/disponibilidad.html')
